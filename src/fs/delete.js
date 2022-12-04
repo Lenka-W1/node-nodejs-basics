2eb752f6c31079af12792847755a8f14a1ecebd4
lenka-w1 <lenka-w1@mail.ru>
2022-12-05 00:59:17 +0300

    feat: add a solution for the delete.js task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,14 @@
+import * as fs from 'fs/promises';
+
+const pathName = 'src/fs/files/fileToRemove.txt'
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await fs.rm(pathName);
+  }
+  catch {
+    throw new Error('FS operation failed');
+  }
 };
 
 await remove();
