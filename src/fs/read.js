4ad818632bfff6c9e6fd4c67f95fdfa3cdf01d0d
lenka-w1 <lenka-w1@mail.ru>
2022-12-05 02:00:06 +0300

    feat: add a solution for the read.js task

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,12 @@
+import * as fs from 'fs';
+
+const files = 'src/fs/files/fileToRead.txt';
+
 const read = async () => {
-    // Write your code here 
+  fs.readFile(files, 'utf8', (err, data) => {
+    if (err) throw new Error('FS operation failed');
+    console.log(data);
+  });
 };
 
 await read();
