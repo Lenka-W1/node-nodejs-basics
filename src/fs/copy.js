fa7a50c1be7d2e4def3a300094559b256b8d3b1a
lenka-w1 <lenka-w1@mail.ru>
2022-12-04 23:55:55 +0300

    feat: add a solution for the copy.js task

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,29 @@
+import * as fs from 'fs';
+import { copyFile } from 'fs/promises';
+import { readdir } from 'fs/promises';
+import { join } from 'path';
+
+const files_copy = 'src/fs/files_copy';
+const files = 'src/fs/files';
+
+fs.access(files, (err) => {
+  if (err) throw new Error('FS operation failed');
+});
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    fs.mkdir(files_copy, (err) => {
+      if (err) throw new Error('FS operation failed');
+    });
+
+    const readDir = await readdir(files);
+
+    for (const file of readDir) {
+      await copyFile(join(files, file), join(files_copy, file))
+    }
+  } catch (err) {
+    if (err) throw err;
+  }
 };
 
 copy();
