b1c38652522fb175ff9bbbfcae69345fb277401b
lenka-w1 <lenka-w1@mail.ru>
2022-12-05 22:51:54 +0300

    feat: add a solution for the stream/read.js task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,10 @@
+import * as fs from 'fs';
+import { stdout } from 'process';
+
+const stream = fs.createReadStream('src/streams/files/fileToRead.txt', 'utf8');
+
 const read = async () => {
-    // Write your code here 
+  stream.pipe(stdout);
 };
 
 await read();
